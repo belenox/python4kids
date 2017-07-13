05d9c03fa8946cbbafed21492b0b3e5a3bfcced7
Student <student@Ben-Lenoxs-MacBook-Air.local>
2017-07-13 11:01:38 -0400

    Chap2 finished

--- Chap2.py
+++ Chap2.py
@@ -1,1 +1,29 @@
-print "Hello World"
+print 8 * 3.57
+print 10 * 365
+print 20 + 3650
+print 3 * 52
+print 3670 - 156
+print 5 + 30 * 20
+print (5+30) * 20
+print ((5 + 30) * 20) / 10
+print 5 + 30 * 20 / 10
+fred = 100
+print fred
+fred = 200
+print fred
+print fred
+john = fred
+print john
+number_of_coins = 200
+print number_of_coins
+print 20 + 10 * 365
+print 3 * 52
+print 3670 - 156
+print 20 + 10 * 365 - 3 * 52
+found_coins = 20
+magic_coins = 10
+stolen_coins = 3
+print found_coins + magic_coins * 365 - stolen_coins * 52
+stolen_coins = 2
+magic_coins = 13
+print found_coins + magic_coins * 365 - stolen_coins * 52
