185410d8ea9f8ef24faf8bd831fb3abf9fba31ed
Student <student@Ben-Lenoxs-MacBook-Air.local>
2017-07-14 14:46:51 -0400

    finished Chapter 3

---
+++ Chap3.py
@@ -0,0 +1,87 @@
+fred = "Why do gorrillas have big nostrils? Big Fingers!!"
+print fred
+fred = "What is pink and fluffy? Pink fluff!!"
+print fred
+fred = "How do dinosaurs pay their bills"
+fred = '''How do dinosaurs pay their bills?
+With tyrannosaurus checks!'''
+print fred
+silly_string  = '''He said, "Aren't can't shouldn't wouldn't."'''
+silly_string  = '''"He said, "Aren't can't shouldn't wouldn't.""'''
+silly_string  = '''He said, "Aren't can't shouldn't wouldn't."'''
+single_quote_str = 'He said "Aren\'t can\'t shouldn\'t wouldn\'t"'
+double_quote_str = "He said \"Aren't can't shouldn't wouldn't\""
+print single_quote_str
+print double_quote_str
+myscore = 1000
+message = 'I scored %s points'
+print message % myscore
+joke_text = '%s: a device for finding furniture in the dark'
+bodypart1 = 'Knee'
+bodypart2 = 'Shin'
+print joke_text % bodypart1
+print joke_text % bodypart2
+nums = 'What did the number %s say to the number %s? Nice belt!!'
+print nums % (0,8)
+print 10 * "a"
+spaces = ' ' * 25
+print '%s 12 Butts Wynd' % spaces
+print '%s Twinklebottom Health' % spaces
+print '%s West Snoring' % spaces
+print
+print
+print 'Dear Sir'
+print
+print 'I wish to report that tiles are missing from the'
+print 'outside toilet roof.'
+print 'I think it was bad wind the other night that blew them away.'
+print
+print 'Regards'
+print 'Malcolm Dithering'
+print 1000 * "snirt"
+wizard_list = 'spider legs, toe of frog, eye of newt, bat wing, slug butter, snake dandruff'
+print wizard_list
+wizard_list = ['spider legs', 'toe of frog', 'eye of newt', 'bat wing', 'slug butter', 'snake dandruff']
+print wizard_list
+print wizard_list[2]
+wizard_list[2] = 'snail tongue'
+print wizard_list
+print wizard_list[2:5]
+some_numbers = [1, 2, 5, 10, 20]
+some_strings = ['Which', 'Witch', 'Is', 'Which']
+numbers_and_strings = ['Why', 'was', 6, 'afraid', 'of', 7, 'because', 7, 8, 9]
+print numbers_and_strings
+numbers = [1, 2, 3, 4]
+strings = ['I', 'kicked', 'my', 'toe', 'and', 'it', 'is', 'sore']
+mylist = [numbers, strings]
+print mylist
+wizard_list.append('bear burp')
+print wizard_list
+wizard_list.append('mandrake')
+wizard_list.append('hemlock')
+wizard_list.append('swamp gas')
+print wizard_list
+del wizard_list[5]
+print wizard_list
+del wizard_list[8]
+del wizard_list[7]
+del wizard_list[6]
+print wizard_list
+list1 = [1, 2, 3, 4]
+list2 = ['I', 'tripped', 'over', 'and', 'hit', 'the', 'floor']
+print list1 + list2
+list1 = [1, 2, 3, 4]
+list2 = ['I', 'ate', 'chocolate', 'and', 'I', 'want', 'more']
+list3 = list1 + list2
+print list3
+list1 = [1, 2]
+print list1 * 5
+fibs = (0, 1, 1, 2, 3)
+print fibs[3]
+favorite_sports = ['Ralph Williams, Football', 'Michael Tippett, Basketball', 'Edward Elgar, Baseball', 'Rebecca Clarke, Netball', 'Ethel Smyth, Badminton', 'Frank Bridge, Rugby']
+favorite_sports = {'Ralph Williams': 'Football', 'Michael Tippett' : 'Basketball', 'Edward Elgar' : 'Baseball', 'Rebecca Clarke' : 'Netball', 'Ethel Smyth' : 'Badminton', 'Frank Bridge' : 'Rugby'}
+print favorite_sports['Rebecca Clarke']
+del favorite_sports['Ethel Smyth']
+print favorite_sports
+favorite_sports['Ralph Williams'] = 'Ice Hockey'
+print favorite_sports
