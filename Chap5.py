36be2d9c3d24bfda67abeb2992f366c5224a08bf
Ben <blenox@shortridgeacademy.com>
2017-07-17 14:56:29 -0400

    Chapter 5 finished

---
+++ Chap5.py
@@ -0,0 +1,75 @@
+age = 13
+if age > 20:
+    print "You are too old!"
+age = 25
+if age > 20:
+    print "You are too old!"
+    print "Why are you here?"
+    print "Why aren't you mowing a lawn or sorting papers?"
+age = 10
+if age > 10:
+    print "You are too old for my jokes!"
+if age >= 10:
+    print "You are too old for my jokes!"
+if age == 10:
+    print "What's brown and sticky?  A stick!!"
+
+print "Want to hear a dirty joke?"
+age = 12
+if age == 12:
+    print "A pig fell in the mud"
+else:
+    print "Shh.  It's a secret."
+
+print "Want to hear a dirty joke?"
+age = 8
+if age == 12:
+    print "A pig fell in the mud"
+else:
+    print "Shh.  It's a secret."
+
+age = 12
+if age == 10:
+    print "What do you call an unhappy cranberry?"
+    print "A blueberry!"
+elif age == 11:
+    print "What did the green grape say to the blue grape?"
+    print "Breathe!  Breathe!"
+elif age == 12:
+    print "What did 0 say to 8?"
+    print "Hi guys!"
+elif age == 13:
+    print "Why wasn't 10 afraid of 7?"
+    print "Because rather than eating 9, 7 8 pi."
+else:
+    print "Huh?"
+if age == 10 or age == 11 or age == 12 or age == 13:
+    print "What is 13 + 49 + 84 + 155 + 97? A headache!"
+else:
+    print "Huh?"
+if age >= 10 and age <= 13:
+    print "What is 13 + 49 + 84 + 155 + 97? A headache!"
+else:
+    print "Huh?"
+
+myval = None
+print myval
+if myval == None:
+    print "The variable myval doesn't have a value"
+age = 10
+if age == 10:
+    print "What's the best way to speak to a monster?"
+    print "From as far away as possible!"
+age = '10'
+if age == 10:
+    print "What's the best way to speak to a monster?"
+    print "From as far away as possible!"
+converted_age = int(age)
+converted_age = str(age)
+converted_age = int(age)
+if converted_age == 10:
+    print "What's the best way to speak to a monster?"
+    print "From as far away as possible!"
+age = '10.5'
+converted_age= float(age)
+print converted_age
